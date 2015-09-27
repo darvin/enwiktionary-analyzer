9f3e2cf93cc900b1b3fe5b657f0cb8f4771cef9c
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-27 01:39:02 -0700

    initial

---
+++ index.js
@@ -0,0 +1,43 @@
+var Parsoid = require('parsoid');
+
+var ls = {
+	setup: function(pc) {
+		pc.debug = true;
+		pc.fetchTemplates = false;
+		pc.expandExtensions = false;
+		pc.fetchConfig = false;
+		pc.fetchImageInfo = false;
+		pc.loadWMF =false;
+		pc.usePHPPreProcessor =false;
+		pc.defaultWiki = "enwiktionary";
+		pc.setMwApi({
+			prefix:"enwiki",
+			uri:"wikiisnotavailable.com/something.php"
+		})
+	},
+};
+
+module.exports.parse = function(wikitext, callback) {
+	Parsoid.parse(wikitext, {
+			pdoc: true,
+			wt2html: false,
+			config: ls
+		}).then(function(pdoc) {
+		console.log("DONE!");
+		console.log(pdoc);
+			String(pdoc).should.equal(text);
+			var templates = pdoc.filterTemplates();
+			templates.length.should.equal(1);
+			String(templates[0]).should.equal('{{foo|bar|baz|eggs=spam}}');
+			var template = templates[0];
+			template.name.should.equal('foo');
+			template.name = 'notfoo';
+			String(template).should.equal('{{notfoo|bar|baz|eggs=spam}}');
+			template.params.length.should.equal(3);
+			template.params[0].name.should.equal('1');
+			template.params[1].name.should.equal('2');
+			template.params[2].name.should.equal('eggs');
+			String(template.get(1).value).should.equal('bar');
+			String(template.get('eggs').value).should.equal('spam');
+		}).done();;
+}
---
+++ test/analyzerTests.js
@@ -0,0 +1,75 @@
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var wiktParser = require('../');
+var path = require('path');
+var fs = require('fs');
+
+
+describe('wiktionary parser', function() {
+
+  xdescribe('parses "test"', function () {
+  	var r = null;
+  	before(function(done) {
+			var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "test.wiki"), {encoding:'utf8'});
+
+      wiktParser.parse(wikitext, function(err, result) {
+        r = result;
+        done(err);
+      });	  
+    });
+
+    it('should parse wiki', function () {
+      expect(r).to.be.ok;
+    });
+    it('should have multiple languages');
+    describe('parsed english word', function() {
+    	var w = null;
+    	before(function() {
+    		w = r["eng"];
+    	});
+    	it('should have 2 meanings');
+    	describe('meaning 1', function() {
+    		it('should have etymology');
+    		it('should have noun role');
+    		it('should have verb role');
+    	});
+    	describe('meaning 2', function() {
+    		it('should have etymology');
+    		it('should have noun role');
+    		it('should have verb role');
+    	});
+
+
+
+    })
+  });
+
+  describe('parses "sample"', function () {
+  	var r = null;
+  	before(function(done) {
+			var wikitext = fs.readFileSync(path.join(__dirname,"fixtures", "sample.wiki"), {encoding:'utf8'});
+
+      wiktParser.parse(wikitext, function(err, result) {
+        r = result;
+        done(err);
+      });   
+    });
+
+    it('should parse wiki', function () {
+      expect(r).to.be.ok;
+    });
+    describe('parsed english word', function() {
+    	var w = null;
+    	before(function() {
+    		w = r["eng"];
+    	});
+    	it('should have 1 meaning');
+    	describe('meaning 1', function() {
+    		it('should have noun role');
+    	});
+    
+
+    })
+  });
+});
